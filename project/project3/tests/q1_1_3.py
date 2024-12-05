9608b68a35df8223a6b3100253d40f9bb34b700b
Hellenpac <83333990+Hellenpac@users.noreply.github.com>
2024-12-04 20:41:17 -0800

    Update q1_1_3.py

--- project/project3/tests/q1_1_3.py
+++ project/project3/tests/q1_1_3.py
@@ -1,3 +1,4 @@
+OK_FORMAT = True
 test = {   'name': 'q1_1_3',
     'points': [0],
     'suites': [{'cases': [{'code': '>>> type(longest_uncut) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
